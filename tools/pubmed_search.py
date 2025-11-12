3f6df22f2f26af9215e33b789c20b222d0d9a301
Jay Warfield <jaywarfield@gmail.com>
2025-11-12 18:58:22 -0500

    Revert "Update pubmed_search.py"
    This reverts commit 5c32486b62ba66495858b7623393854b1eb60068.

--- tools/pubmed_search.py
+++ tools/pubmed_search.py
@@ -1,6 +1,5 @@
 # pubmed_search.py
 from ibm_watsonx_orchestrate.agent_builder.tools import tool
-import requests
 
 @tool
 def pubmed_search(query: str) -> str:
@@ -8,14 +7,4 @@ def pubmed_search(query: str) -> str:
   Search the pubmed repo.
   """
 
-  url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-  params = {
-    "db": "pubmed",
-    "term": query,
-    "retmode": "json",
-    "retmax": 5
-  }
-
-  response = requests.get(url, params=params)
-  ids = response.json()["esearchresult"]["idlist"]
-  return f"PubMed IDs: {', '.join(ids)}"
+  return query
